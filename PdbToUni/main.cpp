e63bad6bec0f099cef29edfba2217fbe155e69de
Robbe Bryssinck <robbebryssinck@gmail.com>
2023-05-10 17:35:16 +0200

    Read target from command line options

--- PdbToUni/main.cpp
+++ PdbToUni/main.cpp
@@ -19,10 +19,19 @@ int main(int argc, char* argv[])
 {
   InitializeLogger();
 
+  if (argc != 2)
+  {
+    spdlog::info("Usage: {} [path_to_pdb]", argv[0]);
+    exit(1);
+  }
+
+  std::string target = argv[1];
+  auto pUsymResult = DiaInterface::CreateUsymFromFile(target.c_str());
+
   //auto pUsymResult = DiaInterface::CreateUsymFromFile(R"(C:\Users\Someone\Desktop\rs-module-lexer-main\rs-module-lexer-main\target\debug\binding.pdb)");
   //auto pUsymResult = DiaInterface::CreateUsymFromFile(R"(C:\dev\crafting_interpreters_rust\target\debug\crafting_interpreters_rust.pdb)");
   //auto pUsymResult = DiaInterface::CreateUsymFromFile(R"(C:\Users\Someone\source\repos\TestApp1\x64\Debug\TestApp1.pdb)");
-  auto pUsymResult = DiaInterface::CreateUsymFromFile(R"(C:\dev\rust_args\target\debug\rust_args.pdb)");
+  //auto pUsymResult = DiaInterface::CreateUsymFromFile(R"(C:\dev\rust_args\target\debug\rust_args.pdb)");
   //auto pUsymResult = DiaInterface::CreateUsymFromFile(R"(C:\dev\rust_sample\target\debug\rust_sample.pdb)");
 
   if (!pUsymResult)
@@ -34,9 +43,13 @@ int main(int argc, char* argv[])
   USYM& usym = pUsymResult.value();
 
   usym.SetSerializer(ISerializer::Type::kJson);
+
+  std::string output = target.substr(0, target.find_last_of("."));
+  auto result = usym.Serialize(output.c_str());
+
   //auto result = usym.Serialize(R"(C:\Users\Someone\Desktop\rs-module-lexer-main\rs-module-lexer-main\target\debug\binding)");
   //auto result = usym.Serialize(R"(C:\dev\crafting_interpreters_rust\target\debug\crafting_interpreters_rust)");
   //auto result = usym.Serialize(R"(C:\Users\Someone\source\repos\TestApp1\x64\Debug\TestApp1)");
-  auto result = usym.Serialize(R"(C:\dev\rust_args\target\debug\rust_args)");
+  //auto result = usym.Serialize(R"(C:\dev\rust_args\target\debug\rust_args)");
   //auto result = usym.Serialize(R"(C:\dev\rust_sample\target\debug\rust_sample)");
 }
